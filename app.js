aa42eae83d98d6f23b449b9d4d7ab3657a7df989
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-12-14 12:59:39 +0700

    up

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = '8080';
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.log(req.user)
+    res.status(200).json({message: 'get success'})
+})
+
+app.listen(
+    PORT,
+    () => console.log(`server is online on http://localhost:${PORT}`)
+)
+
+
+
+
+
+
+
+
+
+
+
