03e8d6191e93ffedf8c328f8d88e08a73c5ff442
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-12-14 14:08:22 +0700

    change

--- controllers/UploadController.js
+++ controllers/UploadController.js
@@ -2,6 +2,10 @@ module.exports = {
     upload: async (req, res) => {
         try {
             const {image} = req.files
+            const allowType = ['image/jpeg', 'image/png']
+            if (allowType.find(type => type == image.mimetype)) throw 'File type not support'
+
+            const naming = require('crypto').randomBytes
             return res.status(200).json({
                 status: true,
                 message: 'ok'
